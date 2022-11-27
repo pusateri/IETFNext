e02454bc51f6b079cf3430e56c7654835fc5f158
Tom Pusateri <pusateri@bangj.com>
2022-11-27 16:37:25 -0500

    Initial Commit

---
+++ IETFNext/IETFNextApp.swift
@@ -0,0 +1,20 @@
+//
+//  IETFNextApp.swift
+//  IETFNext
+//
+//  Created by Tom Pusateri on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct IETFNextApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
